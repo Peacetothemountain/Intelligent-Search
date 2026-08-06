c969af4a6c73894407692d263e67871d0371c0f7
NB Designs <support.nbdesigns@gmail.com>
2026-08-06 01:49:11 -0400

    Bump versionCode to 64

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.pixel.intelligentsearch"
         minSdk = 31
         targetSdk = 37
-        versionCode = 63
+        versionCode = 64
         versionName = "7.5"
     }
 

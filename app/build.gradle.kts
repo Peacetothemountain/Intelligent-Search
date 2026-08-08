3a0153743c6356ad49f14967b5aad7700be933fe
NB Designs <support.nbdesigns@gmail.com>
2026-08-08 19:48:34 -0400

    Configure native debugSymbolLevel = FULL for Play Console symbol extraction

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,9 @@ android {
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("release")
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
         }
     }
     compileOptions {

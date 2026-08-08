7894784fbe4d6e0964e742f2c0fbd01d1d24e9cc
NB Designs <support.nbdesigns@gmail.com>
2026-08-08 19:50:58 -0400

    Bump versionCode to 69 for release 7.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.pixel.intelligentsearch"
         minSdk = 31
         targetSdk = 37
-        versionCode = 68
+        versionCode = 69
         versionName = "7.8"
     }
 

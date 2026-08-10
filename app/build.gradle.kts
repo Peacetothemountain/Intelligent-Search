f040d7bfa0772545a0e9907a0ee72640adbeff1c
NB Designs <support.nbdesigns@gmail.com>
2026-08-10 00:03:58 -0400

    Revert search overlay pill container icons to active icon pack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.pixel.intelligentsearch"
         minSdk = 31
         targetSdk = 37
-        versionCode = 94
+        versionCode = 95
         versionName = "7.8"
     }
 

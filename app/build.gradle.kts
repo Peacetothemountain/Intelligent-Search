b93dea37c68091c719dcf281f2ae80fc681799c2
NB Designs <support.nbdesigns@gmail.com>
2026-08-09 23:58:30 -0400

    Ensure shell container search pills strictly use system default icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.pixel.intelligentsearch"
         minSdk = 31
         targetSdk = 37
-        versionCode = 93
+        versionCode = 94
         versionName = "7.8"
     }
 
